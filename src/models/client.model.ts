f12212799e5dc77f154d35d311e6b860e80eb3a5
ben mahmoud aziz <benmahmoud.aziz121@gmail.com>
2021-02-24 13:17:38 +0100

    back-end completed

---
+++ src/models/client.model.ts
@@ -0,0 +1,40 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Client extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  phone: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+
+  constructor(data?: Partial<Client>) {
+    super(data);
+  }
+}
+
+export interface ClientRelations {
+  // describe navigational properties here
+}
+
+export type ClientWithRelations = Client & ClientRelations;
